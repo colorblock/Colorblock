805649d45b0cebd493e3d4231fc6817b45f5af94
colorblock <colorblockart@gmail.com>
2021-05-27 01:16:20 +0800

    feat: add server config code

--- backend/run.sh
+++ backend/run.sh
@@ -2,3 +2,8 @@
 $env:FLASK_APP = "run.py"  
 $env:FLASK_ENV = "development"
 python -m flask run
+
+# for unix
+export FLASK_APP=run.py
+export FLASK_ENV=development
+python -m flask run
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,13 @@
-export const devMode = process.env.NODE_ENV === 'development';
-export const serverUrl = devMode ? 'http://localhost:5000' : '';
+export const devMode = process.env.NODE_ENV;
+const getServerUrl = (devMode) => {
+  switch (devMode) {
+    case 'development':
+      return 'http://colorblockart.com:5000';
+    case 'production':
+      return 'https://colorblock.art';
+    default:
+      return 'http://localhost:5000';
+  }
+};
+export const serverUrl = getServerUrl(devMode);
 export const walletUrl = 'http://127.0.0.1:9467/v1';
